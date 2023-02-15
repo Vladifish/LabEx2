8ecc426fd93025b9c753b11280f4199a4156eb34
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-02-15 17:15:59 +0800

    addtl comments

--- LabEx2.java
+++ LabEx2.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class LabEx2 {
     final static Scanner console = new Scanner(System.in);
 
-    // yes
+    // parang baliktad yung code nito from the given pseudocode
     static String prefixToInfix(String e) {
         String[] tokenizedPrefix = e.split(" ", 0);
         int n = tokenizedPrefix.length;
