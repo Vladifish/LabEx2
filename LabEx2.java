181c97391a95c074c8176506dbea8aa284a1c077
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-02-15 11:01:14 +0800

    creating stacks

--- LabEx2.java
+++ LabEx2.java
@@ -1,9 +1,21 @@
 import java.util.Scanner;
 
 public class LabEx2 {
-    String prefixToInfix(String e) {
+    final static Scanner console = new Scanner(System.in);
+
+    static String prefixToInfix(String e) {
         String[] tokenizedPrefix = e.split(" ", 0);
-        String infix = "something";
+        int n = tokenizedPrefix.length;
+        // I believe the index starts at 1, might have to change
+        Stack<String> mainStack = new Stack<>(n);
+        Stack<String> operatorStack = new Stack<>();
+        String infix = "something"; // will change to something else later
         return infix;
+    }
+
+    public static void main(String[] args) {
+        String prefixExpression = console.nextLine();
+        System.out.println(prefixToInfix(prefixExpression));
+
     }
 }
