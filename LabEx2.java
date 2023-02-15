16618d60b49f4f72378aad12a00a6a05a96391d7
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-02-15 10:45:50 +0800

    tokenized prefix

--- LabEx2.java
+++ LabEx2.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 
 public class LabEx2 {
-    
+    String prefixToInfix(String e) {
+        String[] tokenizedPrefix = e.split(" ", 0);
+        String infix = "something";
+        return infix;
+    }
 }
-
-
