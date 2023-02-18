4297737028c394cb581bb12e50b420f507e567fb
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-02-18 11:28:10 +0800

    Prefix % Precedence

--- LabEx2.java
+++ LabEx2.java
@@ -134,22 +134,44 @@ public class LabEx2 {
             case 1 -> {
                 return switch (x) {
                     case "+", "-" -> 2;
-                    case "*", "/" -> 4;
-                    case "^" -> 5;
+                    case "%" -> 4;
+                    case "*", "/" -> 6;
+                    case "^" -> 7;
                     case ")" -> 0;
                     default -> -999;
                 };
             }
+            /*
+             * case "+", "-" -> 2;
+             * case "*", "/" -> 4;
+             * case "^" -> 5;
+             * case ")" -> 0;
+             * default -> -999;
+             */
             case 2 -> {
                 return switch (x) {
                     case "+", "-" -> 1;
-                    case "*", "/" -> 3;
-                    case "^" -> 6;
+                    case "%" -> 3;
+                    case "*", "/" -> 5;
+                    case "^" -> 8;
                     case ")" -> 9;
                     case "(" -> 10;
                     default -> -999;
                 };
             }
+            /*
+             * case 2 -> {
+             * return switch (x) {
+             * case "+", "-" -> 1;
+             * case "*", "/" -> 3;
+             * case "^" -> 6;
+             * case ")" -> 9;
+             * case "(" -> 10;
+             * default -> -999;
+             * };
+             * }
+             * 
+             */
             case 3 -> {
                 return switch (x) {
                     case "+", "-" -> 2;
